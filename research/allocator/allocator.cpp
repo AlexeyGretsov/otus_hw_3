fcb61cdf82e15d33b8b718bb996bd16ae6d8b0b9
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-03-22 23:20:21 +0300

    clang format files

--- research/allocator/allocator.cpp
+++ research/allocator/allocator.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-	std::cout << "Allocators test\n";
+int main(int argc, char *argv[]) {
+  std::cout << "Allocators test\n";
 
-	return 0;
+  return 0;
 }
