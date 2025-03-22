6bb8f5e60bb1a2dcb26a82ef7c854f749f923fb7
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-03-22 13:28:38 +0300

    Impl operator new tests

--- research/operator_new_overload.cpp
+++ research/operator_new_overload.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <memory>
+#include <cstdlib>
+
+void* operator new (std::size_t size) {
+	std::cout << "new(std::size_t): " << size << std::endl;
+
+	if (void *p = std::malloc(size))
+	{
+		return p;
+	}
+
+	throw std::bad_alloc();
+}
+
+void* operator new[] (std::size_t size) {
+	std::cout << "new[](std::size_t): " << size << std::endl;
+
+	if (void *p = std::malloc(size))
+	{
+		return p;
+	}
+
+	throw std::bad_alloc();
+}
+
+void* operator new (std::size_t size, const std::nothrow_t &nothrow_value) noexcept {
+	std::cout << "new(std::size_t, std::nothrow_t): " << size << std::endl;
+
+	return std::malloc(size);
+}
+
+void operator delete (void *p) {
+	std::cout << "delete()" << std::endl;
+	std::free(p);
+}
+
+void operator delete[] (void *p) {
+	std::cout << "delete[]()" << std::endl;
+	std::free(p);
+}
 
 int main(int argc, char *argv[]) {
   std::cout << "Operator new overload test\n";
+
+	int *i = new int(42);
+	int *arr = new int[42];
+
+	void *mem = ::operator new(42 * sizeof(int));
+	int * arr2 = new (mem) int[42];
+
+	int *i_nt = new(std::nothrow) int(43);
+
+	std::cout << "i = " << *i << std::endl;
+	std::cout << "i_nt = " << *i_nt << std::endl;
+
+	delete i;
+	delete i_nt;
+	delete [] arr;
+	delete [] arr2;
 
   return 0;
 }
