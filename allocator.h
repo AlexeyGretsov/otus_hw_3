98d70942759b5d480229cf927a9d480fd17e8534
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-03-23 23:58:54 +0300

    Format code

--- allocator.h
+++ allocator.h
@@ -47,52 +47,52 @@ constexpr bool operator!=(const implacement_allocator<T> &a1,
 #endif
 
 template <typename T, size_t items_number> class implacement_allocator_n {
-  public:
-    using value_type = T;
-  
-    static size_t pos;
-    static constexpr size_t size = sizeof(T) * items_number;
-    static uint8_t data[size];
-  
-    implacement_allocator_n() noexcept {}
-    ~implacement_allocator_n() {}
-  
-    template <typename U, size_t u_items_number>
-    implacement_allocator_n(const implacement_allocator_n<U, u_items_number> &) noexcept {}
-  
-    template<class U>
-    struct rebind
-    {
-      using other = implacement_allocator_n<U, items_number>;
-    };
+public:
+  using value_type = T;
 
-    T *allocate(size_t n) {
-      if (pos + n > size)
-        throw std::bad_alloc();
-  
-      size_t cur = pos;
-      pos += n;
-      return reinterpret_cast<T *>(data) + cur;
-    }
-  
-    void deallocate(T *p, size_t n) {}
+  static size_t pos;
+  static constexpr size_t size = sizeof(T) * items_number;
+  static uint8_t data[size];
+
+  implacement_allocator_n() noexcept {}
+  ~implacement_allocator_n() {}
+
+  template <typename U, size_t u_items_number>
+  implacement_allocator_n(
+      const implacement_allocator_n<U, u_items_number> &) noexcept {}
+
+  template <class U> struct rebind {
+    using other = implacement_allocator_n<U, items_number>;
   };
-  
-  template <typename T, size_t items_number> 
-  uint8_t implacement_allocator_n<T, items_number>::data[size];
-  
-  template <typename T, size_t items_number> 
-  size_t implacement_allocator_n<T, items_number>::pos = 0;
-  
-  template <class T, class U, size_t items_number_comp>
-  constexpr bool operator==(const implacement_allocator_n<T, items_number_comp> &a1,
-                            const implacement_allocator_n<U, items_number_comp> &a2) noexcept {
-    return true;
+
+  T *allocate(size_t n) {
+    if (pos + n > size)
+      throw std::bad_alloc();
+
+    size_t cur = pos;
+    pos += n;
+    return reinterpret_cast<T *>(data) + cur;
   }
-  
-  template <class T, class U, size_t items_number_comp>
-  constexpr bool operator!=(const implacement_allocator_n<T, items_number_comp> &a1,
-                            const implacement_allocator_n<U, items_number_comp> &a2) noexcept {
-    return false;
-  }
-  
+
+  void deallocate(T *p, size_t n) {}
+};
+
+template <typename T, size_t items_number>
+uint8_t implacement_allocator_n<T, items_number>::data[size];
+
+template <typename T, size_t items_number>
+size_t implacement_allocator_n<T, items_number>::pos = 0;
+
+template <class T, class U, size_t items_number_comp>
+constexpr bool
+operator==(const implacement_allocator_n<T, items_number_comp> &a1,
+           const implacement_allocator_n<U, items_number_comp> &a2) noexcept {
+  return true;
+}
+
+template <class T, class U, size_t items_number_comp>
+constexpr bool
+operator!=(const implacement_allocator_n<T, items_number_comp> &a1,
+           const implacement_allocator_n<U, items_number_comp> &a2) noexcept {
+  return false;
+}
--- main.cpp
+++ main.cpp
@@ -6,20 +6,24 @@
 #include "allocator.h"
 #include "new.h"
 
+namespace {
+const size_t MAX_ITEMS_COUNT = 10;
+} // namespace
+
 int main(int argc, char *argv[]) {
 
   try {
-    auto m10 = std::map<int, int, std::less<int>,
-                      implacement_allocator_n<std::pair<const int, int>, 10>>{};
+    auto m10 = std::map<
+        int, int, std::less<int>,
+        implacement_allocator_n<std::pair<const int, int>, MAX_ITEMS_COUNT>>{};
 
     for (int i = 0; i < 10; ++i) {
       m10[i] = i;
     }
 
-    std::for_each(m10.begin(), m10.end(), 
-                  [](const std::pair<int, int> &p) {
-                    std::cout << p.first << " -> " << p.second << std::endl;
-                  });
+    std::for_each(m10.begin(), m10.end(), [](const std::pair<int, int> &p) {
+      std::cout << p.first << " -> " << p.second << std::endl;
+    });
     std::cout << "\n";
 
   } catch (const std::exception &e) {
